4f0df8ad5da4455a20b8332c3ddf861f93446fc7
Felipe S <rfelipe273@gmail.com>
2021-11-28 19:58:55 -0300

    teste

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,31 @@
 
-const string: string = '';
-const booleano: boolean = true;
-const numero: number = 0;
-const simbolo: symbol = Symbol();
-const nulo: null = null;
-const indefinido: undefined = undefined;
+// Types 
+const str: string = '';
+const bool: boolean = true;
+const num: number = 0;
+const sim: symbol = Symbol();
+const nul: null = null;
+const undef: undefined = undefined;
 
 
-console.log(string, booleano, numero, simbolo, nulo, indefinido);
+const arr: Array<string> = [];
+const tuple: [number, string] = [1, 'Nome'];
+
+enum Theme {
+    Dark = 'dark',
+    Light = 'light'
+}
+
+const any: any = true;
+
+
+// Functions
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+function ret_never (): never {
+    while(true) {}
+}
+
