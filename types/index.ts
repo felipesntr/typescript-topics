4991b43cfe1b34c93c7b69fdfac6e216f51e6bb9
Felipe S <rfelipe273@gmail.com>
2021-11-28 19:52:40 -0300

    first commit

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+
+const string: string = '';
+const booleano: boolean = true;
+const numero: number = 0;
+const simbolo: symbol = Symbol();
+const nulo: null = null;
+const indefinido: undefined = undefined;
+
+
+console.log(string, booleano, numero, simbolo, nulo, indefinido);
